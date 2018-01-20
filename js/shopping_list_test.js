a1ecc96fbf57cc052f7e58ef3946094888d00931
nick <nicnhines3@gmail.com>
2018-01-19 14:51:22 -1000

    made test 2

--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -21,4 +21,9 @@ describe('ShoppingListItem',function () {
   it('Should be a class', function () {
     expect(ShoppingListItem).to.be.a('function');
   });
+  it('Should have a parameter name', function () {
+      let apple = new ShoppingListItem('Apple');
+      apple.name.should.equal('Apple');
+  });
+
 });
