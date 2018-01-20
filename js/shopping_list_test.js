02b88401776cfebefc7ba214eaa404886efa7201
Zubin Dodhi <zubindodhi@Zubins-MacBook-Pro.local>
2018-01-19 14:26:24 -1000

    first test

---
+++ js/shopping_list_test.js
@@ -0,0 +1,24 @@
+var expect = chai.expect;
+var should = chai.should();
+
+before(function () {
+
+});
+
+after(function () {
+  
+});
+
+beforeEach(function () {
+
+});
+
+afterEach(function () {
+
+});
+
+describe('ShoppingListItem',function () {
+  it('Should be a class', function () {
+    expect(ShoppingListItem).to.be.a('class');
+  });
+});
